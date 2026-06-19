5ddc749d74fb0efdb7098563091fa3909a5f8649
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-06-19 10:43:37 +0200

    refactor: simplify argument validation and usage message in main function

--- src/main.c
+++ src/main.c
@@ -1,54 +1,23 @@
 #include "cub3d.h"
 
-error_exit(char *message)
+static void	usage(void)
 {
-	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd(message, 2);
-	ft_putchar_fd('\n', 2);
-	exit(EXIT_FAILURE);
-}
-static int validate_arguments(int argc, char **argv)
-{
-	char *ext;
-	char *name;
-	size_t len;
-
-	if (argc != 2)
-		error_exit(ERR_INVALID_ARG);
-	ext = ".cub";
-	name = ft_strrchr(argv[1], '/');
-	if (name)
-		name++;
-	else
-		name = argv[1];
-	len = ft_strlen(name);
-	if (len <= FILE_EXT_LENGTH || name[0] == '.')
-		error_exit(ERR_INVALID_EXT);
-	if (ft_strncmp(name, ext, FILE_EXT_LENGTH) != 0)
-		if (ft_revstrncmp(name, ext, FILE_EXT_LENGTH) != 0)
-			error_exit(ERR_INVALID_EXT);
-	return (0);
+	ft_putstr_fd("Usage: ./cub3d <map.cub>\n", 2);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	char **map;
+	t_game	game;
 
+	ft_bzero(&game, sizeof(t_game));
 	if (argc != 2)
 	{
-		printf("Usage: ./cub3D <map.cub>\n");
+		usage();
 		return (1);
 	}
-	validate_arguments(argc, argv);
-	map = parse_map(argv[1]);
-	if (!map)
-	{
-		printf("Error\nInvalid map\n");
+	if (!parse_config(&game, argv[1]))
 		return (1);
-	}
-
-	print_map(map);
-	free_map(map);
-
+	print_config(&game);
+	free_config(&game);
 	return (0);
 }
