70e0feb362466eb5b6fe5c75eddcf4e6d7078704
Lukas Benedikt Wittwer <lwittwer@c1r4p2.42vienna.com>
2026-07-14 23:16:17 +0200

    add: moving forward and backwards

--- src/game/handle_input.c
+++ src/game/handle_input.c
@@ -6,35 +6,55 @@
 /*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 20:25:49 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/07/14 22:49:09 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/07/14 23:13:37 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "cub3d.h"
-/*
 static int	is_walkable(t_cub *c, int y, int x)
 {
-	double	i;
-	double	j;
+	int	map_x;
+	int	map_y;
 
-	i = c->player->x;
-	j = c->player->y;
-	if (c->config->map[(int)i][(int)j] == '1')
+	map_x = (int)x;
+	map_y = (int)y;
+	if (c->config->map[map_y][map_x] == '1')
 		return (0);
+	return (1);
 	
 }
-*/
 
 int	handle_keypress(int keycode, t_cub *c)
 {
+	double	new_x;
+	double	new_y;
+
 	if (keycode == 65307)
 	{
 		printf("ESC: %d pressed\n", keycode);
 		on_close(c);
 	}
 	else if (keycode == 65362 || keycode == 119)
+	{
 		printf("UP: %d pressed\n", keycode);
+		new_x = c->player->x + c->player->dir_x * MOVE_SPEED;
+		new_y = c->player->y + c->player->dir_y * MOVE_SPEED;
+		if (is_walkable(c, new_y, new_x))
+		{
+			c->player->x = new_x;
+			c->player->y = new_y;
+		}
+	}
 	else if (keycode == 65364 || keycode == 115)
+	{
 		printf("DOWN: %d pressed\n", keycode);
+		new_x = c->player->x - c->player->dir_x * MOVE_SPEED;
+		new_y = c->player->y - c->player->dir_y * MOVE_SPEED;
+		if (is_walkable(c, new_y, new_x))
+		{
+			c->player->x = new_x;
+			c->player->y = new_y;
+		}
+	}
 	else if (keycode == 65361 || keycode == 97)
 		printf("LEFT: %d pressed\n", keycode);
 	else if (keycode == 65363 || keycode == 100)
