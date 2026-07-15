dc289906feee07b1450cc0c8dbf13b9dc9e74d28
Lukas Benedikt Wittwer <lwittwer@c1r4p4.42vienna.com>
2026-07-15 16:56:53 +0200

    improv: move function for any movement instead of calculating every direciton itself

--- src/game/handle_input.c
+++ src/game/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 20:25:49 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/07/15 16:45:11 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/07/15 16:54:05 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "cub3d.h"
@@ -20,13 +20,24 @@ static int	is_walkable(t_cub *c, int y, int x)
 	if (c->config->map[map_y][map_x] == '1')
 		return (0);
 	return (1);
-	
+}
+
+static void	move(t_cub *c, double dx, double dy)
+{
+	double	new_x;
+	double	new_y;
+
+	new_x = c->player->x + dx;
+	new_y = c->player->y + dy;
+	if (is_walkable(c, new_y, new_x))
+	{
+		c->player->x = new_x;
+		c->player->y = new_y;
+	}
 }
 
 int	handle_keypress(int keycode, t_cub *c)
 {
-	double	new_x;
-	double	new_y;
 
 	if (keycode == 65307)
 	{
@@ -36,46 +47,22 @@ int	handle_keypress(int keycode, t_cub *c)
 	else if (keycode == 65362 || keycode == 119)
 	{
 		printf("UP: %d pressed\n", keycode);
-		new_x = c->player->x + c->player->dir_x * MOVE_SPEED;
-		new_y = c->player->y + c->player->dir_y * MOVE_SPEED;
-		if (is_walkable(c, new_y, new_x))
-		{
-			c->player->x = new_x;
-			c->player->y = new_y;
-		}
+		move(c, c->player->dir_x * MOVE_SPEED, c->player->dir_y * MOVE_SPEED);
 	}
 	else if (keycode == 65364 || keycode == 115)
 	{
 		printf("DOWN: %d pressed\n", keycode);
-		new_x = c->player->x - c->player->dir_x * MOVE_SPEED;
-		new_y = c->player->y - c->player->dir_y * MOVE_SPEED;
-		if (is_walkable(c, new_y, new_x))
-		{
-			c->player->x = new_x;
-			c->player->y = new_y;
-		}
+		move(c, -c->player->dir_x * MOVE_SPEED, -c->player->dir_y * MOVE_SPEED);
 	}
 	else if (keycode == 65361 || keycode == 97)
 	{
 		printf("LEFT: %d pressed\n", keycode);
-		new_x = c->player->x + c->player->dir_y * MOVE_SPEED;
-		new_y = c->player->y - c->player->dir_x * MOVE_SPEED;
-		if (is_walkable(c, new_y, new_x))
-		{
-			c->player->x = new_x;
-			c->player->y = new_y;
-		}
+		move(c, c->player->dir_y * MOVE_SPEED, -c->player->dir_x * MOVE_SPEED);
 	}
 	else if (keycode == 65363 || keycode == 100)
 	{
 		printf("RIGHT: %d pressed\n", keycode);
-		new_x = c->player->x - c->player->dir_y * MOVE_SPEED;
-		new_y = c->player->y + c->player->dir_x * MOVE_SPEED;
-		if (is_walkable(c, new_y, new_x))
-		{
-			c->player->x = new_x;
-			c->player->y = new_y;
-		}
+		move(c, -c->player->dir_y * MOVE_SPEED, c->player->dir_x * MOVE_SPEED);
 	}
 	else if (keycode == 113)	//left
 		rotate_player(c, -ROT_SPEED);
