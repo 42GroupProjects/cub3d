b4931fe58c458b5771697bb6575f7f6fa8fdf9ae
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-19 20:00:12 +0200

    style: standardize formatting in includes.h

--- includes/includes.h
+++ includes/includes.h
@@ -1,13 +1,13 @@
 #ifndef INCLUDES_H
-# define INCLUDES_H
+#define INCLUDES_H
 
-# include "mlx.h"
-# include <stdio.h>
-# include <stdlib.h>
-# include <math.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include "../libft/include/libft.h"
-# include "errors.h"
+#include "mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "libft.h"
+#include "errors.h"
 
 #endif
