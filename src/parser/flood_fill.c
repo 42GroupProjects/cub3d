013fdad36ce7d579b0d31984b8919ceaac4d4674
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-19 20:15:32 +0200

    refactor: remove validate_map_borders check from validate_map_layout function

--- src/parser/flood_fill.c
+++ src/parser/flood_fill.c
@@ -78,8 +78,6 @@ int validate_map_layout(t_game *game)
         return (parse_error(ERR_MAP_INVALID));
     if (!validate_player_count(game->map))
         return (parse_error(ERR_MAP_NO_PLAYER));
-    if (!validate_map_borders(game))
-        return (parse_error(ERR_MAP_NOT_CLOSED));
     copy = dup_map(game->map, game->height);
     if (!copy)
         return (oom_error());
