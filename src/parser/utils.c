c1997b21b236d70820e505bb1ce8c8b20e1fafb0
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-19 19:59:51 +0200

    style: update comments for clarity and remove redundant documentation in utils.c

--- src/parser/utils.c
+++ src/parser/utils.c
@@ -6,16 +6,12 @@
 /*   By: thanh-ng <thanh-ng@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 19:02:28 by thanh-ng          #+#    #+#             */
-/*   Updated: 2026/07/19 19:03:11 by thanh-ng         ###   ########.fr       */
+/*   Updated: 2026/07/19 19:14:04 by thanh-ng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/**
- * Print "Error\n<msg>" to stderr and return FAILURE, so a caller can write:
- *   return (parse_error(ERR_...));   // bad input / invalid map
- */
 int parse_error(char *msg)
 {
 	ft_putstr_fd("Error\n", 2);
@@ -24,11 +20,6 @@ int parse_error(char *msg)
 	return (FAILURE);
 }
 
-/**
- * Print the allocation-failure message and return OOM, kept distinct from
- * parse_error so the caller can tell "out of memory" from "bad input":
- *   return (oom_error());
- */
 int oom_error(void)
 {
 	ft_putstr_fd("Error\n", 2);
@@ -37,7 +28,6 @@ int oom_error(void)
 	return (OOM);
 }
 
-/** Advance past leading spaces/tabs and return the new position. */
 char *skip_spaces(char *s)
 {
 	while (*s == ' ' || *s == '\t')
@@ -45,10 +35,6 @@ char *skip_spaces(char *s)
 	return (s);
 }
 
-/**
- * TRUE if `line` begins with `id` followed by a space/tab. Guards against
- * "NORTH" being mistaken for the "NO" identifier.
- */
 int match_id(char *line, char *id)
 {
 	int len;
@@ -61,7 +47,6 @@ int match_id(char *line, char *id)
 	return (TRUE);
 }
 
-/** TRUE if `line` is empty or contains only whitespace. */
 int is_blank(char *line)
 {
 	int i;
