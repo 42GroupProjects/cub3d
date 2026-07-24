dc4a161bb7a826c8bab9aa3bf3209d77169d2813
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-24 20:54:44 +0200

    feat: dim screen for full-map overlay

---
+++ src/game/minimap_full.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minimap_full.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thanh-ng <thanh-ng@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/21 20:30:00 by thanh-ng          #+#    #+#             */
+/*   Updated: 2026/07/21 20:45:00 by thanh-ng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static void	draw_dim_screen(t_cub *cub)
+{
+	int	x;
+	int	y;
+
+	cub->mm_alpha = MM_FULL_ALPHA;
+	y = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			mm_put_pixel_blend(cub, x, y, 0x000000);
+			x++;
+		}
+		y++;
+	}
+}
