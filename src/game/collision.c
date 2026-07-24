4739eb143d05b1e56ed01c9ea69f9df362e025f8
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-24 21:51:02 +0200

    style: format code for consistency and readability

--- src/game/collision.c
+++ src/game/collision.c
@@ -5,29 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thanh-ng <thanh-ng@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/24 21:25:00 by thanh-ng          #+#    #+#             */
-/*   Updated: 2026/07/24 21:25:00 by thanh-ng         ###   ########.fr       */
+/*   Created: 2026/07/24 21:44:56 by thanh-ng          #+#    #+#             */
+/*   Updated: 2026/07/24 21:44:57 by thanh-ng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	cell_blocked(t_cub *c, int map_x, int map_y)
+static int cell_blocked(t_cub *c, int map_x, int map_y)
 {
-	if (map_x < 0 || map_y < 0
-		|| map_y >= c->config->height
-		|| map_x >= c->config->width)
+	if (map_x < 0 || map_y < 0 || map_y >= c->config->height || map_x >= c->config->width)
 		return (1);
-	if (c->config->map[map_y][map_x] == '1'
-		|| c->config->map[map_y][map_x] == ' ')
+	if (c->config->map[map_y][map_x] == '1' || c->config->map[map_y][map_x] == ' ')
 		return (1);
 	return (0);
 }
 
-static int	is_walkable(t_cub *c, double y, double x)
+static int is_walkable(t_cub *c, double y, double x)
 {
-	double	dx;
-	double	dy;
+	double dx;
+	double dy;
 
 	dy = -COLLISION_PAD;
 	while (dy <= COLLISION_PAD)
@@ -44,10 +41,10 @@ static int	is_walkable(t_cub *c, double y, double x)
 	return (1);
 }
 
-void	move_player(t_cub *c, double dx, double dy)
+void move_player(t_cub *c, double dx, double dy)
 {
-	double	new_x;
-	double	new_y;
+	double new_x;
+	double new_y;
 
 	new_x = c->player->x + dx;
 	new_y = c->player->y + dy;
