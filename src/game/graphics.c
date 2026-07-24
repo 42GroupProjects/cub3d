1d8f976ad205a3ab20771b337d90e9a55e8dbe93
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-24 20:54:44 +0200

    feat: show full-map overlay when toggled

--- src/game/graphics.c
+++ src/game/graphics.c
@@ -145,6 +145,8 @@ int	render(t_cub *cub)
 		x++;
 	}
 	draw_radar_minimap(cub);
+	if (cub->show_full_map)
+		draw_full_map_overlay(cub);
 	mlx_put_image_to_window(cub->mlx, cub->win, cub->img, 0, 0);
 	return (SUCCESS);
 }
