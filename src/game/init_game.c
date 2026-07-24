ed92837c12c71cba35a242a443d83c7a816848c7
Trung Nguyen <sendtotrungnguyen@outlook.com>
2026-07-24 19:47:15 +0200

    fix: tear down mlx on init_game failure paths

--- src/game/init_game.c
+++ src/game/init_game.c
@@ -6,7 +6,7 @@
 /*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/21 16:35:44 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/07/24 18:49:12 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/07/24 19:50:00 by thanh-ng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ static int	init_ray(t_cub *c)
 	if (!ray)
 		return (FAILURE);
 	ft_memset(ray, 0, sizeof(t_ray));
-	print_ray_struct(ray);
-	// FIXME: heap ray is unused — cast_ray uses a stack t_ray; free this or use it
 	c->ray = ray;
 	return (SUCCESS);
 }
@@ -32,22 +30,20 @@ int	init_game(t_cub *c, t_game *g)
 	c->mlx = mlx_init();
 	if (!c->mlx)
 		return (parse_error(ERR_MLX_INIT));
-	c->win = mlx_new_window(c->mlx, WIDTH, HEIGHT, "cub3d");
+	c->win = mlx_new_window(c->mlx, WIDTH, HEIGHT, "cub3D");
 	if (!c->win)
-		return (parse_error(ERR_MLX_WINDOW));
+		return (free_cub_struct(c), parse_error(ERR_MLX_WINDOW));
 	c->img = mlx_new_image(c->mlx, WIDTH, HEIGHT);
 	if (!c->img)
-		return (parse_error(ERR_MLX_IMAGE));
+		return (free_cub_struct(c), parse_error(ERR_MLX_IMAGE));
 	c->addr = mlx_get_data_addr(c->img, &c->bpp, &c->line_len, &c->endian);
 	if (!c->addr)
-		return (parse_error(ERR_MLX_DATA));
-	// TODO: load NO/SO/WE/EA via mlx_xpm_file_to_image here (paths in c->config)
+		return (free_cub_struct(c), parse_error(ERR_MLX_DATA));
 	if (init_player(c) != SUCCESS)
-		printf("%s failed\n", __func__);
+		return (free_cub_struct(c), parse_error(ERR_MAP_NO_PLAYER));
 	if (init_ray(c) != SUCCESS)
-		printf("%s failed\n", __func__);
+		return (free_cub_struct(c), oom_error());
 	if (init_textures(c) != SUCCESS)
-		printf("%s failed\n", __func__);
-	// FIXME: on any fail above, destroy mlx/win/img and return FAILURE (still returns SUCCESS)
+		return (free_cub_struct(c), parse_error(ERR_MLX_IMAGE));
 	return (SUCCESS);
 }
