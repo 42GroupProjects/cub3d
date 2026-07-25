c1c7b3c08c97e11ba57c11cc13f7bb01670c4472
Lukas Benedikt Wittwer <lwittwer@c1r4p4.42vienna.com>
2026-07-25 17:33:33 +0200

    rm: 1 function which was outdated

--- src/game/utils.c
+++ src/game/utils.c
@@ -3,29 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
+/*   By: lwittwer <lwittwer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 18:14:31 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/07/25 14:39:04 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/07/25 16:56:44 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-/* OUTDATED funtion
-int	get_wall_color(t_ray *r)
-{
-	if (r->side == 0)
-	{
-		if (r->step_x > 0)
-			return (0xFF0000);
-		return (0x00FF00);
-	}
-	if (r->step_y > 0)
-		return (0x0000FF);
-	return (0xFFFF00);
-}
-*/
 
 /* Select the correct wall texture based on the DDA result.
 * - side == 0: vertical wall (east or west)
